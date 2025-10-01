f04af3133a7f92c3f50088c0a258582741faf112
gokalp <ggurocak58@gmail.com>
2025-10-01 17:02:15 +0300

    Initial Commit

---
+++ MapsUsage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MapsUsage
+//
+//  Created by Gökalp Gürocak on 1.10.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MapsUsageTests/MapsUsageTests.swift
@@ -0,0 +1,17 @@
+//
+//  MapsUsageTests.swift
+//  MapsUsageTests
+//
+//  Created by Gökalp Gürocak on 1.10.2025.
+//
+
+import Testing
+@testable import MapsUsage
+
+struct MapsUsageTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
